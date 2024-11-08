f3729f536da5b64a8365114f330d86662a9916fd
AyeAreEm <ag.ayeareem@gmail.com>
2024-11-08 15:15:30 +0800

    update jumping sys

--- runner/runner_movement.gd
+++ runner/runner_movement.gd
@@ -4,8 +4,13 @@ const LEFT_LANE = -6.0
 const MIDDLE_LANE = 0.0
 const RIGHT_LANE = 6.0
 
-@export var gravity = 10.0
-@export var jump_velocity = 1000.0
+@export var jump_height = 5.0
+@export var jump_time_apex = 0.5
+@export var jump_time_fall = 0.5
+
+@onready var jump_velocity = (2.0 * jump_height) / jump_time_apex
+@onready var jump_gravity = (-2.0 * jump_height) / (jump_time_apex * jump_time_apex)
+@onready var fall_gravity = (-2.0 * jump_height) / (jump_time_fall * jump_time_fall)
 
 @export var distance_threshold = 50
 @export var speed = 15.0
@@ -15,21 +20,22 @@ var target_position = position
 
 func _ready():
 	pass
-	#velocity = Vector3(0, 0, 1)
 
 func handle_speed(delta):
 	if round(abs(position.z)) as int % distance_threshold == 0:
 		speed += 0.2
-		print(speed)
 		target_position.z -= speed * delta
 	else:
 		target_position.z -= speed * delta
 
 func handle_jump(delta):
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y += jump_velocity * delta
+		velocity.y = jump_velocity
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y += get_gravity() * delta
+		
+func get_gravity() -> float:
+	return jump_gravity if velocity.y > 0.0 else fall_gravity
 
 func _process(delta):
 	handle_speed(delta)
@@ -51,6 +57,7 @@ func _process(delta):
 		else:
 			target_position.x = target_position.x
 
-	position = lerp(position, target_position, 0.1)
+	position.x = lerp(position.x, target_position.x, 0.1)
+	position.z = lerp(position.z, target_position.z, 0.1)
 
 	move_and_slide()
